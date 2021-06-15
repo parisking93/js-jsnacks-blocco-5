3bb091d64e95e80464d88988ef17d1d512573175
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-06-15 16:06:20 +0200

    add secondo script

--- js/script.js
+++ js/script.js
@@ -38,4 +38,46 @@ if (biciPesoMin.length < 1) {
         console.log(biciPesoMin[i].nome + ':  ' + biciPesoMin[i].peso + 'kg');
     }
 }
-  
+
+
+// 2 script 
+// Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà:
+// Punti fatti e falli subiti
+
+var squadre = [
+    {
+        nome : 'juve',
+        punti : 0,
+        falli : 0
+    },
+    {
+        nome : 'milan',
+        punti : 0,
+        falli : 0
+    },
+    {
+        nome : 'inter',
+        punti : 0,
+        falli : 0
+    }
+];
+
+for (var i = 0; i < squadre.length; i++) {
+    squadre[i].punti = rand(1,5);
+    squadre[i].falli = rand(1,100);
+}
+
+for(var i = 0; i < squadre.length; i++) {
+    console.log('nome: ' + squadre[i].nome + ', punti: ' + squadre[i].punti + ', falli fatti: ' + squadre[i].falli);
+}
+
+
+
+
+
+function rand(min,max) {
+    var nRandom = Math.floor((Math.random() * ((max + 1) - min) + min))
+    return nRandom
+}
